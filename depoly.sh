b1fb11f5da28825c5e729dc8541088bd483f3483
Thu Ya Kyaw <thuyakyaw@yahoo.com>
2018-10-28 11:58:41 +0800

    added rm line in depoly.sh

--- depoly.sh
+++ depoly.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install apache2
+sudo apt install apache2 -y
 
 rm /var/www/html/index.html
 mv GeeksHacking.github.io/* /var/www/html/.
+rm -r GeeksHacking.github.io
